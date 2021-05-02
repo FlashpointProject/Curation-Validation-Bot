0e8f8d87a9c06d409899a5333ae13a1d7f55328f
curation-bot-guy <laytonyon@gmail.com>
2021-05-01 20:50:30 -0400

    Fix for Flash in meta.txts

--- curation_validator.py
+++ curation_validator.py
@@ -284,8 +284,8 @@ def validate_curation(filename: str) -> tuple[list, list, Optional[bool], Option
         if props.get("Library") == "theatre":
             curation_type = CurationType.ANIMATION
         else:
-            platform = props.get("Platform")
-            if platform == "Flash" or platform is None:
+            platform: Optional[str] = props.get("Platform")
+            if "Flash" in platform or platform is None:
                 curation_type = CurationType.FLASH_GAME
             else:
                 curation_type = CurationType.OTHER_GAME
