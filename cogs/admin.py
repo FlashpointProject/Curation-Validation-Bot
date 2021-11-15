5478b2f997305f3c1744000abe6ed93b6b634c50
curation-bot-guy <laytonyon@gmail.com>
2021-11-14 19:30:47 -0500

    Added admin.py

---
+++ cogs/admin.py
@@ -0,0 +1,130 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+import asyncio
+import importlib
+import os
+import re
+import subprocess
+import sys
+
+import discord
+from discord.ext import commands
+
+
+class Admin(commands.Cog):
+    """Admin-only commands that make the bot dynamic."""
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    async def run_process(self, command):
+        try:
+            process = await asyncio.create_subprocess_shell(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            result = await process.communicate()
+        except NotImplementedError:
+            process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            result = await self.bot.loop.run_in_executor(None, process.communicate)
+
+        return [output.decode() for output in result]
+
+    @commands.command(hidden=True)
+    async def load(self, ctx, *, module):
+        """Loads a module."""
+        try:
+            self.bot.load_extension(module)
+        except commands.ExtensionError as e:
+            await ctx.send(f'{e.__class__.__name__}: {e}')
+        else:
+            await ctx.send('\N{OK HAND SIGN}')
+
+    @commands.command(hidden=True)
+    async def unload(self, ctx, *, module):
+        """Unloads a module."""
+        try:
+            self.bot.unload_extension(module)
+        except commands.ExtensionError as e:
+            await ctx.send(f'{e.__class__.__name__}: {e}')
+        else:
+            await ctx.send('\N{OK HAND SIGN}')
+
+    @commands.group(name='reload', hidden=True, invoke_without_command=True)
+    async def _reload(self, ctx, *, module):
+        """Reloads a module."""
+        try:
+            self.bot.reload_extension(module)
+        except commands.ExtensionError as e:
+            await ctx.send(f'{e.__class__.__name__}: {e}')
+        else:
+            await ctx.send('\N{OK HAND SIGN}')
+
+    _GIT_PULL_REGEX = re.compile(r'\s*(?P<filename>.+?)\s*\|\s*[0-9]+\s*[+-]+')
+
+    def find_modules_from_git(self, output):
+        files = self._GIT_PULL_REGEX.findall(output)
+        ret = []
+        for file in files:
+            root, ext = os.path.splitext(file)
+            if ext != '.py':
+                continue
+
+            if root.startswith('cogs/'):
+                # A submodule is a directory inside the main cog directory for
+                # my purposes
+                ret.append((root.count('/') - 1, root.replace('/', '.')))
+
+        # For reload order, the submodules should be reloaded first
+        ret.sort(reverse=True)
+        return ret
+
+    def reload_or_load_extension(self, module):
+        try:
+            self.bot.reload_extension(module)
+        except commands.ExtensionNotLoaded:
+            self.bot.load_extension(module)
+
+    @_reload.command(name='all', hidden=True)
+    async def _reload_all(self, ctx):
+        """Reloads all modules, while pulling from git."""
+
+        async with ctx.typing():
+            stdout, stderr = await self.run_process('git pull')
+
+        # progress and stuff is redirected to stderr in git pull
+        # however, things like "fast forward" and files
+        # along with the text "already up-to-date" are in stdout
+
+        if stdout.startswith('Already up to date.'):
+            return await ctx.send(stdout)
+
+        modules = self.find_modules_from_git(stdout)
+        mods_text = '\n'.join(f'{index}. `{module}`' for index, (_, module) in enumerate(modules, start=1))
+        ctx.send(f'Updating modules:\n{mods_text}')
+
+        statuses = []
+        for is_submodule, module in modules:
+            if is_submodule:
+                try:
+                    actual_module = sys.modules[module]
+                except KeyError:
+                    statuses.append(('<:greyTick:563231201280917524>', module))
+                else:
+                    try:
+                        importlib.reload(actual_module)
+                    except Exception as e:
+                        statuses.append(('<:redTick:330090723011592193>', module))
+                    else:
+                        statuses.append(('<:greenTick:330090705336664065>', module))
+            else:
+                try:
+                    self.reload_or_load_extension(module)
+                except commands.ExtensionError:
+                    statuses.append(('<:redTick:330090723011592193>', module))
+                else:
+                    statuses.append(('<:greenTick:330090705336664065>', module))
+
+        await ctx.send('\n'.join(f'{status}: `{module}`' for status, module in statuses))
+
+def setup(bot):
+    bot.add_cog(Admin(bot))
