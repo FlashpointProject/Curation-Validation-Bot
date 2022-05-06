11bd46e92541d54a1f8056445585d07e5b08d942
Dri0m <dri0m@seznam.cz>
2022-05-06 16:45:42 +0200

    add path provider endpoint

--- validator-server.py
+++ validator-server.py
@@ -16,14 +16,17 @@ app = FastAPI()
 @app.post("/upload")
 async def create_upload_file(response: Response, file: UploadFile = File(...)):
     l.debug(f"received file '{file.filename}'")
+
     base_path = tempfile.mkdtemp(prefix="curation_validator_")
     new_filepath = base_path + "/file" + pathlib.Path(file.filename).suffix
+
     with open(new_filepath, "wb") as dest:
         l.debug(f"copying file '{file.filename}' into '{new_filepath}'.")
         shutil.copyfileobj(file.file, dest)
     try:
         curation_errors, curation_warnings, is_extreme, curation_type, meta, image_dict = validate_curation(
             new_filepath)
+
     except Exception as e:
         response.status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
         return {
@@ -47,6 +50,34 @@ async def create_upload_file(response: Response, file: UploadFile = File(...)):
         "images": image_dict
     }
 
+
+# just hand over absolute path to the file instead of uploading it, saves some unnecessary copying ay?
+@app.post("/provide-path")
+async def provide_file(response: Response, path: str):
+    try:
+        l.debug(f"validating provided file '{path}'")
+        curation_errors, curation_warnings, is_extreme, curation_type, meta, image_dict = validate_curation(path)
+
+    except Exception as e:
+        response.status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
+        return {
+            "exception": "".join(
+                traceback.format_exception(
+                    etype=type(e), value=e, tb=e.__traceback__
+                )
+            )
+        }
+
+    return {
+        "filename": pathlib.Path(path).name,
+        "path": path,
+        "curation_errors": curation_errors,
+        "curation_warnings": curation_warnings,
+        "is_extreme": is_extreme,
+        "curation_type": curation_type,
+        "meta": meta,
+        "images": image_dict
+    }
 
 # TODO this does not return all valid tags because the wiki page sucks
 @app.get("/tags")
