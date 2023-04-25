3f308c5749b32afa60b5b81e2a85056fcb54d92d
Colin <colinb969@gmail.com>
2023-04-25 20:35:42 +0100

    Use backlisted_tags.json

--- curation_validator.py
+++ curation_validator.py
@@ -304,17 +304,22 @@ def validate_curation(filename: str) -> tuple[list,
                     warnings.append(f"Tag `{tag}` is not a known tag, please verify (did you write it correctly?).")
 
         extreme: tuple[str, bool] = ("Extreme", bool(props.get("Extreme")))
+        blacklisted_tags = get_blacklisted_tag_list_file()
         extreme_tags = get_extreme_tag_list_file()
+        is_blacklisted = False
         is_extreme = False
         if extreme[1] and (props["Extreme"] == "Yes" or props["Extreme"] is True):
             is_extreme = True
         if tags:
             has_extreme_tags = bool([tag for tag in tags if tag in extreme_tags])
+            has_blacklisted_tags = bool([tag for tag in tags if tag in blacklisted_tags])
             has_legacy_extreme = "LEGACY-Extreme" in tags
-            if has_extreme_tags or has_legacy_extreme:
+            if has_blacklisted_tags or has_extreme_tags or has_legacy_extreme:
                 is_extreme = True
             if is_extreme and not has_extreme_tags:
                 errors.append("Curation is extreme but lacks extreme tags.")
+            if has_blacklisted_tags:
+                errors.append("Contains blacklisted tags")
 
         if props.get("Library") is not None and "theatre" in props.get("Library"):
             curation_type = CurationType.ANIMATION
@@ -391,10 +396,16 @@ def get_tag_list_file() -> list[dict[str, str]]:
         data = json.load(f)
         return data["tags"]
 
+@cached(cache=TTLCache(maxsize=1, ttl=3600))
+def get_blacklisted_tag_list_file() -> list[str]:
+    l.debug(f"getting blacklisted tags from file...")
+    with open("data/blacklisted_tags.json", "r", encoding="utf-8") as f:
+        data = json.load(f)
+        return data["tags"]
 
 @cached(cache=TTLCache(maxsize=1, ttl=3600))
 def get_extreme_tag_list_file() -> list[str]:
-    l.debug(f"getting tags from file...")
+    l.debug(f"getting extreme tags from file...")
     with open("data/extreme_tags.json", "r", encoding="utf-8") as f:
         data = json.load(f)
         return data["tags"]
